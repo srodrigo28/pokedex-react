5923a09961bd960f5e43b0da409e03469c41e68a
Sebastião Rodrigo <rodrigoexer1@gmail.com>
2025-03-13 22:48:16 -0300

    udpated

--- src/pokedex1/index.tsx
+++ src/pokedex1/index.tsx
@@ -1,7 +1,17 @@
-export function Pokedex1(){
-    return(
-        <div>
-        
+export function Pokedex1() {
+    return (
+        <div className="card-container">
+            <div className="box-code bg-grass">
+                <span className="text-code">#0001</span>
+            </div>
+            <img src="./image/bulbasaur.png" alt="" />
+            <div className="box-type">
+                <span className="text-type bg-grass">Grass</span>
+                <span className="text-type bg-poison">Poison</span>
+            </div>
+            <div className="box-name bg-grass">
+                <span className="text-name">Bulbasaur</span>
+            </div>
         </div>
     )
 }
